e8a679859f8ff2d8335d3d661496fe856c4ba779
Mauro Druwel <mauro.druwel@gmail.com>
2024-08-03 15:56:17 +0200

    Start screen

--- start-services.sh
+++ start-services.sh
@@ -6,5 +6,14 @@ service ssh start
 # Start XRDP service
 service xrdp start
 
+# Start the XFCE session
+startxfce4 &
+
+# Wait for a few seconds to ensure XFCE has started
+sleep 5
+
+# Start Thunar file manager
+thunar &
+
 # Keep the container running
 tail -f /dev/null
